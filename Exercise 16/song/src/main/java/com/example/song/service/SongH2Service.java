5cdf2ccbdbf094bdd9bb23649453970ad639e248
tjmanoj <manojtofficial@gmail.com>
2024-08-29 15:08:22 +0000

    song v2 completed

--- Exercise 16/song/src/main/java/com/example/song/service/SongH2Service.java
+++ Exercise 16/song/src/main/java/com/example/song/service/SongH2Service.java
@@ -18,14 +18,14 @@ public class SongH2Service implements SongRepository {
 
     @Override
     public ArrayList<Song> getSongs() {
-        List<Song> songs = db.query("SELECT * FROM playlist", new SongRowMapper());
+        List<Song> songs = db.query("SELECT * FROM PLAYLIST", new SongRowMapper());
         return new ArrayList<Song>(songs);
     }
 
     @Override
     public Song getSongById(Integer songId) {
         try {
-            return db.queryForObject("SELECT * FROM playlist WHERE songId=?", new SongRowMapper(), songId);
+            return db.queryForObject("SELECT * FROM PLAYLIST WHERE songId=?", new SongRowMapper(), songId);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
@@ -33,9 +33,9 @@ public class SongH2Service implements SongRepository {
 
     @Override
     public Song addSong(Song newSong) {
-        db.update("Insert into playlist(songName,lyricist,singer,musicDirector) values(?,?,?,?)", newSong.getSongName(),
+        db.update("Insert into PLAYLIST(songName,lyricist,singer,musicDirector) values(?,?,?,?)", newSong.getSongName(),
                 newSong.getLyricist(), newSong.getSinger(), newSong.getMusicDirector());
-        Song song = db.queryForObject("select * from playlist where songName = ?", new SongRowMapper(),
+        Song song = db.queryForObject("Select * from PLAYLIST where songName=?", new SongRowMapper(),
                 newSong.getSongName());
         return song;
     }
@@ -44,16 +44,16 @@ public class SongH2Service implements SongRepository {
     public Song updateSongById(Integer songId, Song newSong) {
         try {
             if (newSong.getSongName() != null) {
-                db.update("Update playlist set songName = ? where sondId = ?", newSong.getSongName(), songId);
+                db.update("Update PLAYLIST set songName = ? where songId = ?", newSong.getSongName(), songId);
             }
             if (newSong.getSinger() != null) {
-                db.update("Update playlist set singer =? where songId =?", newSong.getSinger(), songId);
+                db.update("Update PLAYLIST set singer =? where songId =?", newSong.getSinger(), songId);
             }
             if (newSong.getLyricist() != null) {
-                db.update("Update playlist set lyricist =? where songId =?", newSong.getLyricist(), songId);
+                db.update("Update PLAYLIST set lyricist =? where songId =?", newSong.getLyricist(), songId);
             }
             if (newSong.getMusicDirector() != null) {
-                db.update("Update playlist set musicDirector =? where songId =?", newSong.getMusicDirector(), songId);
+                db.update("Update PLAYLIST set musicDirector =? where songId =?", newSong.getMusicDirector(), songId);
             }
 
             return getSongById(songId);
@@ -65,7 +65,7 @@ public class SongH2Service implements SongRepository {
     @Override
     public void deleteSongById(Integer songId) {
         try {
-            db.update("DELETE FROM playlist WHERE songId =?", songId);
+            db.update("DELETE FROM PLAYLIST WHERE songId =?", songId);
             throw new ResponseStatusException(HttpStatus.OK);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.OK);
